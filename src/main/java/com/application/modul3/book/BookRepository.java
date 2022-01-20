d946624c94be6cdf84c070eec4cddf52f02e6b71
MonicaCraciun <a.monicacraciun@yahoo.ro>
2022-01-20 20:45:02 +0200

    First commit of the project(user, book implementation)

---
+++ src/main/java/com/application/modul3/book/BookRepository.java
@@ -0,0 +1,7 @@
+package com.application.modul3.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+
+}
---
+++ src/main/java/com/application/modul3/book/BookService.java
@@ -0,0 +1,34 @@
+package com.application.modul3.book;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	public Book createBook(Book book) {
+		return bookRepository.saveAndFlush(book);
+	}
+	
+	public List<Book> getAllBooks() {
+		return bookRepository.findAll();
+	}
+	
+	public Book getBookById(Integer id) {
+		Optional <Book> bookOpt = bookRepository.findById(id);
+		if(bookOpt.isPresent()) {
+			return bookOpt.get();
+		}
+		return null;
+	}
+	
+	public void deleteBookById(Integer id) {
+		bookRepository.deleteById(id);
+	}
+}
